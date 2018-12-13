a50da64d648bbf662b433e7ec27d291d9c3491d4
Tomasz Hodorek <tomaszh@dip.co.uk>
2018-12-13 18:02:48 +0100

    cors configuration

--- HouseholdExpensesTrackerServer21.Web/Startup.cs
+++ HouseholdExpensesTrackerServer21.Web/Startup.cs
@@ -36,6 +36,8 @@ namespace HouseholdExpensesTrackerServer21.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
                 {
@@ -74,6 +76,12 @@ namespace HouseholdExpensesTrackerServer21.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+                builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
